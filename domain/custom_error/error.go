5d1ae397c698e69a133c8890bdb0becf5c840507
Manuel Zavala <majezanu@gmail.com>
2022-06-04 15:30:03 -0500

    Refactor: Improve error response handling

--- domain/custom_error/error.go
+++ domain/custom_error/error.go
@@ -1,6 +1,30 @@
 package custom_error
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
 type ErrorResponse struct {
 	Error string `json:"error" example:"message"`
 	Code  int    `json:"code" example:"400"`
+}
+
+func NewErrorResponse(err error) ErrorResponse {
+	httpStatusCode := http.StatusInternalServerError
+	switch {
+	case errors.Is(err, PokemonIdFormatError) || errors.Is(err, BadPokemonFieldError) || errors.Is(err, PokemonAlreadyExistError):
+		httpStatusCode = http.StatusUnprocessableEntity
+	case errors.Is(err, PokemonNotFoundError):
+		httpStatusCode = http.StatusNotFound
+	case errors.Is(err, PokemonFileCantBeOpen):
+		httpStatusCode = http.StatusBadRequest
+	default:
+		httpStatusCode = http.StatusInternalServerError
+	}
+	return ErrorResponse{
+		Error: fmt.Sprint(err),
+		Code:  httpStatusCode,
+	}
 }
